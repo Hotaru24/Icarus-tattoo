2ccc01dcf1f77923c885c8303d46f09693ae6431
Hotaru24 <florent-pasquet@protonmail.com>
2020-08-26 12:32:55 +0200

    about template Page non-static image build-test try3

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -5,8 +5,8 @@ import Layout from '../components/Layout';
 import Content, { HTMLContent } from '../components/Content';
 import  {gsap} from "gsap";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
-import soulignement from "../img/soulignement.png";
-import img1 from "../img/aboutImg1.png";
+import soulignement from "../../static/img/soulignement.png";
+import img1 from "../../static/img/aboutImg1.png";
 import "../Style/aboutPage.css";
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
