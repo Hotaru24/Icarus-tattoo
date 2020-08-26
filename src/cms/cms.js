ba9b909bb2640491be0a60b9c8e56668de083f6c
Hotaru24 <florent-pasquet@protonmail.com>
2020-08-26 16:12:23 +0200

    HomePage is changed

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -9,6 +9,5 @@ import IndexPagePreview from './preview-templates/IndexPagePreview'
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
-CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
